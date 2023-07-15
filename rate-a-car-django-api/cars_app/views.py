82c4b5e71d93790de1b23fa49912ee4d39a23f3b
wojtek <wojlas93@gmail.com>
2023-07-15 20:24:11 +0200

    global api statuses improved

--- rate-a-car-django-api/cars_app/views.py
+++ rate-a-car-django-api/cars_app/views.py
@@ -1,11 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
 
 from .serializers import BrandSerializer, CarModelsSerializer
 
 from .models import Brand, CarModel
-from rateACarApi.global_methods import throw200, throw500
+from rateACarApi.global_methods import throw200, throw400, throw500
 
 class BrandView(APIView):
     def get(self, request):
@@ -13,9 +12,9 @@ class BrandView(APIView):
         queryset = Brand.objects.all()
         serializer = BrandSerializer(queryset, many=True)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=throw200)
       except Brand.DoesNotExist:
-        return Response(serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(serializer.errors, status=throw500)
       
 class GetCarModels(APIView):
    def get(self, request):
@@ -33,6 +32,6 @@ class CarModelByBrandView(APIView):
         queryset = CarModel.objects.filter(brand_id = id)
         serializer = CarModelsSerializer(queryset, many=True)
 
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=throw200)
       except CarModel.DoesNotExist:
-        return Response(serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response(serializer.errors, status=throw500)
--- rate-a-car-django-api/rateACarApi/global_methods.py
+++ rate-a-car-django-api/rateACarApi/global_methods.py
@@ -3,5 +3,8 @@ from rest_framework import status
 def throw200():
     return status.HTTP_200_OK
 
+def throw400():
+    return status.HTTP_400_BAD_REQUEST
+
 def throw500():
     return status.HTTP_500_INTERNAL_SERVER_ERROR
